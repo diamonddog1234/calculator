41b90848e11fcbf00010c97214055cdefa5d71c3
diamonddog1234@gmail.com <diamonddog1234@gmail.com>
2023-02-17 15:15:03 +0300

    Инициализирующий коммит

---
+++ Калькулятор.py
@@ -0,0 +1,24 @@
+print("To end this app enter: .")
+
+def main():
+
+    while True:
+         symbol = input("Operation sing (+, -, *, /): ")
+         if symbol == '.':
+             break
+         if symbol in ('+', '-', '*', '/'):
+             a = float(input("First number: "))
+             b = float(input("Second number: "))
+             if symbol == '+':
+                 print(a+b)
+             elif symbol == '-':
+                 print(a-b)
+             elif symbol == '*':
+                 print(a*b)
+             elif symbol == '/':
+                 if b != 0:
+                     print(a/b)
+                 else:
+                     print("Please, don't divide by zero to avoid the collapse of Universe!")
+         else:
+             print("Hey, write a supported sign!")
